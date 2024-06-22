256f3f52d6f2476ac3dc6eff7385bb84c8e10a79
ProudDesk <timherdemark86@gmail.com>
2024-06-22 20:11:49 +0200

    comments

--- src/file_io.rs
+++ src/file_io.rs
@@ -3,7 +3,7 @@ use std::io::{self, Read, Write};
 
 pub fn read_file(path: &str) -> io::Result<String> {
     match fs::read_to_string(path) {
-        Ok(c) => Ok(c),
+        Ok(content) => Ok(content),
         Err(e) => Err(e),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::process;
 use regmand::{cli, file_io, regex_processor};
+use std::process;
 
 fn main() {
     let args = cli::get_matches();
 
+    // Get the arguments
     let path = args.file_name.as_str();
     let pattern = args.pattern.as_str();
     let replacement = args.new_text.as_str();
 
+    // The text to replace
     let to_replace = match file_io::read_file(path) {
         Ok(content) => content,
         Err(_) => {
@@ -16,6 +18,7 @@ fn main() {
         }
     };
 
+    // The new text with regex replacement
     let new_text = match regex_processor::process_text(to_replace.as_str(), pattern, replacement) {
         Ok(res) => res,
         Err(e) => {
@@ -23,6 +26,8 @@ fn main() {
             process::exit(1);
         }
     };
+
+    // Determine whether to make a new file or not
     let mut path_to_write = String::new();
     if args.overwrite {
         path_to_write = "replaced-".to_string() + path;
@@ -34,5 +39,10 @@ fn main() {
         process::exit(1);
     }
 
-    println!("regex replace successful!")
+    // Print the information
+    println!("Regex replace successful!");
+    if args.echo {
+        println!("old: {}", to_replace);
+        println!("new: {}", new_text);
+    }
 }
