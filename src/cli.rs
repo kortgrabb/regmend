804e60350367bd1dc909fe060a267316fee3d061
ProudDesk <timherdemark86@gmail.com>
2024-06-22 20:16:45 +0200

    help command description (clap)

--- src/cli.rs
+++ src/cli.rs
@@ -2,14 +2,23 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    /// The file to use as reference
     #[arg(short, long)]
     pub file_name: String,
+
+    /// The regex pattern, use '\' to use eg: quotes
     #[arg(short, long)]
     pub pattern: String,
+
+    /// The text to place in the match
     #[arg(short, long)]
     pub new_text: String,
+
+    /// Whether to overwrite the file or make a new one
     #[arg(short, long)]
     pub overwrite: bool,
+
+    /// Whether to echo the new and old text (does not care about file-size)
     #[arg(short, long)]
     pub echo: bool,
 }
